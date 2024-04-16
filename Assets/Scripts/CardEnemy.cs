88f6ebdf14a6c787b977e31ed634179b5c282de8
sunsart <sunsart7@naver.com>
2024-04-16 16:05:38 +0900

    card.cs 파일삭제함. card 상속 포기함.

--- Assets/Scripts/CardEnemy.cs
+++ Assets/Scripts/CardEnemy.cs
@@ -2,30 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
-public class CardEnemy : Card
+public class CardEnemy : MonoBehaviour
 {
-  [HideInInspector] public int healthVal = 9;
+  [SerializeField] private Sprite frontSprite;
+  [SerializeField] private Sprite backSprite;
+  [SerializeField] private SpriteRenderer spriteRenderer;
+
+  [SerializeField] protected TextMeshPro textHealth;
+
+  protected bool isFlipped = false;   // 앞면으로 뒤집어졌는지 여부
+  protected bool isFlipping = false;  // 앞면으로 뒤집어지는 중인지 여부
+
+  public int healthVal = 9;
 
 
   void Start() 
   {
-    SetCardStat();
-
-    //카드생성시 카드가 뒤집히기전 뒷면상태에서 텍스트가 표시되면 안되기때문
+    // 게임시작시 카드가 뒤집히기전 뒷면상태에서 텍스트가 표시되면 안됨
     textHealth.alpha = 0f;
 
     Invoke("FlipCard", Settings.flipDelay);
   }
 
-  void Update()
-  {
-    //Debug.DrawRay(transform.position, Vector2.left * 2, new Color(1,0,0));
-  }
-
   public void SetCardStat()
   {
     textHealth.text = healthVal.ToString();
+
+    // healthVal 값에 따라 텍스트 visible 조정
+    if(healthVal > 0)
+      textHealth.alpha = 1f;
+    else
+      textHealth.alpha = 0f;
   }
 
   public void ClickedCard() 
@@ -40,4 +49,24 @@ public class CardEnemy : Card
     }
   }
 
+  public void FlipCard() 
+  {
+    isFlipping = true;
+
+    Vector3 originalScale = transform.localScale;
+    Vector3 targetScale = new Vector3(0f, originalScale.y, originalScale.z);
+    transform.DOScale(targetScale, Settings.flipSpeed).OnComplete(() => 
+    {
+      isFlipped = !isFlipped;
+
+      if(isFlipped)
+      {
+        spriteRenderer.sprite = frontSprite;
+        SetCardStat();
+      }
+
+      transform.DOScale(originalScale, Settings.flipSpeed).OnComplete(() => { isFlipping = false; });
+    });
+  }
+  
 }
--- Assets/Scripts/CardPlayer.cs
+++ Assets/Scripts/CardPlayer.cs
@@ -2,18 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
-public class CardPlayer : Card
+public class CardPlayer : MonoBehaviour
 {
+  [SerializeField] private Sprite frontSprite;
+  [SerializeField] private Sprite backSprite;
+  [SerializeField] private SpriteRenderer spriteRenderer;
+
+  [SerializeField] protected TextMeshPro textHealth;
+  [SerializeField] protected TextMeshPro textAttack;
+
+  protected bool isFlipped = false;   // 앞면으로 뒤집어졌는지 여부
+  protected bool isFlipping = false;  // 앞면으로 뒤집어지는 중인지 여부
+
   public int healthVal = 100;
   public int attackVal = 5;
 
 
   void Start() 
   {
-    SetCardStat();
-
-    //게임시작시 카드가 뒤집히기전 뒷면상태에서 텍스트가 표시되면 안되기때문
+    // 게임시작시 카드가 뒤집히기전 뒷면상태에서 텍스트가 표시되면 안됨
     textHealth.alpha = 0f;
     textAttack.alpha = 0f;
 
@@ -25,10 +34,16 @@ public class CardPlayer : Card
     textHealth.text = healthVal.ToString();
     textAttack.text = attackVal.ToString();
 
-    //무기공격력 값에 따라 텍스트 visible 조정
+    // healthVal 값에 따라 텍스트 visible 조정
+    if(healthVal > 0)
+      textHealth.alpha = 1f;
+    else
+      textHealth.alpha = 0f;
+
+    // attackVal 값에 따라 텍스트 visible 조정
     if(attackVal > 0)
       textAttack.alpha = 1f;
-    else if(attackVal <= 0)
+    else
       textAttack.alpha = 0f;
   }
 
@@ -38,5 +53,25 @@ public class CardPlayer : Card
       return;
 
     Debug.Log("주인공 카드 클릭...");
+  }
+
+  public void FlipCard() 
+  {
+    isFlipping = true;
+
+    Vector3 originalScale = transform.localScale;
+    Vector3 targetScale = new Vector3(0f, originalScale.y, originalScale.z);
+    transform.DOScale(targetScale, Settings.flipSpeed).OnComplete(() => 
+    {
+      isFlipped = !isFlipped;
+
+      if(isFlipped)
+      {
+        spriteRenderer.sprite = frontSprite;
+        SetCardStat();
+      }
+
+      transform.DOScale(originalScale, Settings.flipSpeed).OnComplete(() => { isFlipping = false; });
+    });
   }
 }
